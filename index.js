77f142bcc72678b25c07fca460087bca7d920d76
Jon Pastorek <jppastorek@gmail.com>
2023-05-07 14:05:21 -0500

    add new user

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
 const sqlite3 = require("sqlite3").verbose();
 const fs = require("fs");
+import User from "./User";
 
-let db = new sqlite3.Database(
-  "./restaurant.db",
-  sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
-  (err) => {
-    if (err) {
-      return console.error(err.message);
-    }
-    console.log("Successfully connected to the restaurant sqlite database!");
-    fs.readFile("./db_create_tables.sql", "utf8", (err, data) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      db.exec(data, (err) => {
-        if (err) {
-          console.log(err.message);
-        }
-        db.close((err) => {
-          if (err) {
-            console.log(err.message);
-          }
-          console.log("Closed the database connection.");
-        });
-      });
-    });
-  }
-);
+const userController = new User('./restaurant.db');
+
+userController.addNewUser('Jon', 'Pastorek', 'jppastorek@gmail.com', 'password123', 'Kenner');
+
+// let db = new sqlite3.Database(
+//   "./restaurant.db",
+//   sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
+//   (err) => {
+//     if (err) {
+//       return console.error(err.message);
+//     }
+//     console.log("Successfully connected to the restaurant sqlite database!");
+//     fs.readFile("./db_create_tables.sql", "utf8", (err, data) => {
+//       if (err) {
+//         console.error(err);
+//         return;
+//       }
+//       db.exec(data, (err) => {
+//         if (err) {
+//           console.log(err.message);
+//         }
+//         db.close((err) => {
+//           if (err) {
+//             console.log(err.message);
+//           }
+//           console.log("Closed the database connection.");
+//         });
+//       });
+//     });
+//   }
+// );
